71d4c62c510058abb50fed2924ec00d6a995be8f
Simone Bonfrate <info@bonfry.com>
2023-12-29 11:29:00 +0100

    Update dart2ast_engine.dart with new code for parsing and printing AST nodes

--- bin/dart2ast_engine.dart
+++ bin/dart2ast_engine.dart
@@ -1,5 +1,19 @@
-import 'package:dart2ast_engine/dart2ast_engine.dart' as dart2ast_engine;
+import 'package:dart2ast_engine/dart2ast.dart';
+import 'package:antlr4/antlr4.dart';
 
-void main2(List<String> arguments) {
-  print('Hello world: ${dart2ast_engine.calculate()}!');
+void main() {
+  final code = """final a = 1;
+  final b = "ddd";""";
+
+  final input = InputStream.fromString(code);
+  final lexer = DartLexer(input);
+
+  final tokens = CommonTokenStream(lexer);
+  final parser = DartParser(tokens);
+
+  final root = parser.dartFile();
+  print(root.text);
+  for (final child in root.lines()) {
+    print(child.text);
+  }
 }
