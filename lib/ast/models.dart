cc8963db3a299fbd6f33a8026bf249d9695358ec
Simone Bonfrate <info@bonfry.com>
2024-01-07 12:03:39 +0100

    add serialization

--- lib/ast/models.dart
+++ lib/ast/models.dart
@@ -8,6 +8,11 @@ class Point extends Equatable {
 
   @override
   List<Object?> get props => [line, column];
+
+  Map<String, int> toJson() => {
+        'line': line,
+        'column': column,
+      };
 }
 
 class Position extends Equatable {
@@ -15,6 +20,13 @@ class Position extends Equatable {
   final Point end;
 
   Position(this.start, this.end);
+
+  factory Position.fromJson(Map<String, dynamic> json) {
+    return Position(
+      Point(json['start']['line'], json['start']['column']),
+      Point(json['end']['line'], json['end']['column']),
+    );
+  }
 
   @override
   List<Object?> get props => [start, end];
@@ -38,25 +50,55 @@ class Position extends Equatable {
     return start.line >= other.start.line ||
         (start.line == other.start.line && start.column >= other.start.column);
   }
+
+  Map<String, dynamic> toJson() => {
+        'start': start.toJson(),
+        'end': end.toJson(),
+      };
 }
 
 abstract class Node extends Equatable {
   final Position position;
 
   Node(this.position);
+
+  Map<String, dynamic> toJson();
 }
 
-class DartFile extends Node {
+class ProgramFile extends Node {
   final List<Statement> lines;
 
-  DartFile(this.lines, super.position);
+  ProgramFile(this.lines, super.position);
+
+  factory ProgramFile.fromJson(Map<String, dynamic> json) {
+    return ProgramFile(
+      (json['lines'] as List).map((e) => Statement.fromJson(e)).toList(),
+      Position.fromJson(json['position']),
+    );
+  }
 
   @override
   List<Object?> get props => [lines];
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'lines': lines.map((e) => e.toJson()).toList(),
+        'position': position.toJson(),
+      };
 }
 
 abstract class Statement extends Node {
   Statement(super.position);
+
+  factory Statement.fromJson(Map<String, dynamic> json) {
+    return switch (json['nodeType']) {
+      'VariableDeclarationStatement' =>
+        VariableDeclarationStatement.fromJson(json),
+      'AssignmentStatement' => AssignmentStatement.fromJson(json),
+      _ => throw UnimplementedError(),
+    };
+  }
 }
 
 class VariableDeclarationStatement extends Statement {
@@ -73,8 +115,40 @@ class VariableDeclarationStatement extends Statement {
     super.position,
   );
 
+  factory VariableDeclarationStatement.fromJson(Map<String, dynamic> json) {
+    return VariableDeclarationStatement(
+      switch (json['type']) {
+        'variable' => VariableType.variable,
+        'immutable' => VariableType.immutable,
+        'constant' => VariableType.constant,
+        _ => throw UnimplementedError(),
+      },
+      json['name'],
+      switch (json['valueType']) {
+        'Int' => VariableValueType.Int,
+        'Double' => VariableValueType.Double,
+        'String' => VariableValueType.String,
+        'Boolean' => VariableValueType.Boolean,
+        'Reference' => VariableValueType.Reference,
+        _ => throw UnimplementedError(),
+      },
+      Expression.fromJson(json['value']),
+      Position.fromJson(json['position']),
+    );
+  }
+
   @override
   List<Object?> get props => [type, name, valueType, value];
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'type': type.toString(),
+        'name': name,
+        'valueType': valueType.toString(),
+        'value': value.toJson(),
+        'position': position.toJson(),
+      };
 }
 
 enum VariableType { variable, immutable, constant }
@@ -87,36 +161,95 @@ class AssignmentStatement extends Statement {
 
   AssignmentStatement(this.name, this.value, super.position);
 
+  factory AssignmentStatement.fromJson(Map<String, dynamic> json) {
+    return AssignmentStatement(
+      json['name'],
+      Expression.fromJson(json['value']),
+      Position.fromJson(json['position']),
+    );
+  }
+
   @override
   List<Object?> get props => [name, value];
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'name': name,
+        'value': value.toJson(),
+        'position': position.toJson(),
+      };
 }
 
 abstract class Expression extends Node {
   Expression(super.position);
+
+  factory Expression.fromJson(Map<String, dynamic> json) {
+    return switch (json['nodeType']) {
+      'IntLit' => IntLit.fromJson(json),
+      'DecLit' => DecLit.fromJson(json),
+      'StringLit' => StringLit.fromJson(json),
+      'BoolLit' => BoolLit.fromJson(json),
+      _ => throw UnimplementedError(),
+    };
+  }
 }
 
 class IntLit extends Expression {
   final String value;
   IntLit(this.value, super.position);
 
+  factory IntLit.fromJson(Map<String, dynamic> json) {
+    return IntLit(json['value'], Position.fromJson(json['position']));
+  }
+
   @override
   List<Object?> get props => [value];
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'value': value,
+        'position': position.toJson(),
+      };
 }
 
 class DecLit extends Expression {
   final String value;
   DecLit(this.value, super.position);
 
+  factory DecLit.fromJson(Map<String, dynamic> json) {
+    return DecLit(json['value'], Position.fromJson(json['position']));
+  }
+
   @override
   List<Object?> get props => [value];
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'value': value,
+        'position': position.toJson(),
+      };
 }
 
 class StringLit extends Expression {
   final String value;
   StringLit(this.value, super.position);
 
+  factory StringLit.fromJson(Map<String, dynamic> json) {
+    return StringLit(json['value'], Position.fromJson(json['position']));
+  }
+
   @override
   List<Object?> get props => [value];
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'value': value,
+        'position': position.toJson(),
+      };
 }
 
 class BoolLit extends Expression {
@@ -125,4 +258,15 @@ class BoolLit extends Expression {
 
   @override
   List<Object?> get props => [value];
+
+  factory BoolLit.fromJson(Map<String, dynamic> json) {
+    return BoolLit(json['value'], Position.fromJson(json['position']));
+  }
+
+  @override
+  Map<String, dynamic> toJson() => {
+        'nodeType': runtimeType.toString(),
+        'value': value,
+        'position': position.toJson(),
+      };
 }
