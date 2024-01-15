4a63f6bf0506a5350c741fbcf724b5e324cb6fa0
Simone Bonfrate <info@bonfry.com>
2024-01-15 18:12:34 +0100

    add test case for function and classes

--- bin/dart2ast_engine.dart
+++ bin/dart2ast_engine.dart
@@ -1,23 +1,41 @@
+import 'package:dart2ast_engine/dart2ast.dart';
 import 'package:dart2ast_engine/parsing/parser.dart';
+import 'package:indent/indent.dart';
 
 void main() {
   final code = """
-  if( true){
+  class pino{
     final a = 1;
+
+    pino(this.a){
+      var a = 1;
+    }
+    
+    pino.algo(this.a, bool s){
+      var a = 1;
+    }
+
+    int test(int c, bool d){
+      var b = 2;
+    }
   }
   """
-      .trim();
+      .unindent();
   ;
 
   final result = ParserFacade.parseFromText(code);
-  final errorsSerialized = result.errors
-      .map((e) =>
-          "[Ln ${e.position.line}, Col ${e.position.column}] ${e.message}")
-      .toList();
+  if (result.errors.isNotEmpty) {
+    final errorsSerialized = result.errors
+        .map((e) =>
+            "[Ln ${e.position.line}, Col ${e.position.column}] ${e.message}")
+        .toList();
 
-  print("""
+    print("""
   Errors:
   ${errorsSerialized.join('\n')}
   """
-      .trim());
+        .unindent());
+  } else {
+    print(result.root?.Transpile().unindent());
+  }
 }
