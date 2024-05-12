ae177549f3c9191dcbfdd6a37929423f848e1466
Simone Bonfrate <info@bonfry.com>
2024-05-12 19:58:35 +0200

    add comparison expression converter

--- lib/ast/protocol/converter/ast2protobuf.dart
+++ lib/ast/protocol/converter/ast2protobuf.dart
@@ -45,23 +45,30 @@ protocol.Statement convertStatementToProtobuf(ast.Statement object) {
   final statement = protocol.Statement();
 
   final _ = switch (object) {
-    ast.VariableDeclarationStatement s => statement.varDeclarationStatement = s.toProtobuf(),
-    ast.AssignmentStatement s => statement.assignmentStatement = s.toProtobuf(), 
-    ast.ExpressionDefinitionStatement s => statement.expressionDefinitionStatement = s.toProtobuf(),
+    ast.VariableDeclarationStatement s => statement.varDeclarationStatement =
+        s.toProtobuf(),
+    ast.AssignmentStatement s => statement.assignmentStatement = s.toProtobuf(),
+    ast.ExpressionDefinitionStatement s =>
+      statement.expressionDefinitionStatement = s.toProtobuf(),
     ast.ReturnStatement s => statement.returnStatement = s.toProtobuf(),
-    ast.IfDefinitionStatement s => statement.ifDefinitionStatement = s.toProtobuf(),
+    ast.IfDefinitionStatement s => statement.ifDefinitionStatement =
+        s.toProtobuf(),
     ast.IfBlock node => node,
-    ast.WhileDefinitionStatement s => statement.whileDefinitionStatement = s.toProtobuf(),
+    ast.WhileDefinitionStatement s => statement.whileDefinitionStatement =
+        s.toProtobuf(),
     ast.VarDeclarationForStatement node => node,
     ast.AssignmentForStatement node => node,
     ast.ExpressionForStatement node => node,
     ast.ItemDefinition node => node,
-    ast.ForDefinitionStatement s => statement.forDefinitionStatement = s.toProtobuf(),
+    ast.ForDefinitionStatement s => statement.forDefinitionStatement =
+        s.toProtobuf(),
     ast.ForCondition node => node,
     ast.FunctionDefinitionStatement s => UnimplementedError(""),
     ast.Parameter node => node,
-    ast.ClassDefinitionStatement s => statement.classDefinitionStatement = s.toProtobuf(),
-    ast.ObjectPropertyAssignmentStatement s => statement.objectPropertyAssignmentStatement = s.toProtobuf(),
+    ast.ClassDefinitionStatement s => statement.classDefinitionStatement =
+        s.toProtobuf(),
+    ast.ObjectPropertyAssignmentStatement s =>
+      statement.objectPropertyAssignmentStatement = s.toProtobuf(),
     _ => throw UnsupportedError("${object.runtimeType} not supported"),
   };
 
@@ -69,31 +76,47 @@ protocol.Statement convertStatementToProtobuf(ast.Statement object) {
 }
 
 protocol.Expression convertExpressionToProtobuf(ast.Expression object) {
-  final expression =  protocol.Expression();
+  final expression = protocol.Expression();
 
   final _ = switch (object) {
-    ast.IntLit e => expression.intLit = e.toProtobuf(), 
+    ast.IntLit e => expression.intLit = e.toProtobuf(),
     ast.DecLit e => expression.decLit = e.toProtobuf(),
     ast.BoolLit e => expression.boolLit = e.toProtobuf(),
     ast.StringLit e => expression.stringLit = e.toProtobuf(),
-    ast.ListLiteralExpression e => expression.listLiteralExpression = e.toProtobuf(),
-    ast.BinaryMathExpression e => expression.binaryMathExpression = e.toProtobuf(),
-    ast.BinaryLogicExpression e => expression.binaryLogicExpression = e.toProtobuf(),
-    ast.UnaryMathExpression e => expression.unaryMathExpression = e.toProtobuf(),
-    ast.UnaryLogicExpression e => expression.unaryLogicExpression = e.toProtobuf(),
-    ast.PreIncrementExpression e => expression.preIncrementExpression = e.toProtobuf(),
-    ast.PostIncrementExpression e => expression.postIncrementExpression = e.toProtobuf(),
-    ast.PreDecrementExpression e => expression.preDecrementExpression = e.toProtobuf(),
-    ast.PostDecrementExpression e => expression.postDecrementExpression = e.toProtobuf(),
+    ast.ListLiteralExpression e => expression.listLiteralExpression =
+        e.toProtobuf(),
+    ast.BinaryMathExpression e => expression.binaryMathExpression =
+        e.toProtobuf(),
+    ast.BinaryLogicExpression e => expression.binaryLogicExpression =
+        e.toProtobuf(),
+    ast.BinaryComparisonExpression e => expression.binaryComparisonExpression =
+        e.toProtobuf(),
+    ast.UnaryMathExpression e => expression.unaryMathExpression =
+        e.toProtobuf(),
+    ast.UnaryLogicExpression e => expression.unaryLogicExpression =
+        e.toProtobuf(),
+    ast.PreIncrementExpression e => expression.preIncrementExpression =
+        e.toProtobuf(),
+    ast.PostIncrementExpression e => expression.postIncrementExpression =
+        e.toProtobuf(),
+    ast.PreDecrementExpression e => expression.preDecrementExpression =
+        e.toProtobuf(),
+    ast.PostDecrementExpression e => expression.postDecrementExpression =
+        e.toProtobuf(),
     ast.InputExpression e => expression.inputExpression = e.toProtobuf(),
     ast.OutputExpression e => expression.outputExpression = e.toProtobuf(),
-    ast.ParenthesysExpression e => expression.parenthesisExpression = e.toProtobuf(),
-    ast.VarReferenceExpression e => expression.varReferenceExpression = e.toProtobuf(),
-    ast.FunctionCallExpression e => expression.functionCallExpression = e.toProtobuf(),
-    ast.ObjectMethodCallExpression e => expression.objectMethodCallExpression = e.toProtobuf(),
-    ast.ObjectPropertyReferenceExpression e => expression.objectPropertyReferenceExpression = e.toProtobuf(),
+    ast.ParenthesysExpression e => expression.parenthesisExpression =
+        e.toProtobuf(),
+    ast.VarReferenceExpression e => expression.varReferenceExpression =
+        e.toProtobuf(),
+    ast.FunctionCallExpression e => expression.functionCallExpression =
+        e.toProtobuf(),
+    ast.ObjectMethodCallExpression e => expression.objectMethodCallExpression =
+        e.toProtobuf(),
+    ast.ObjectPropertyReferenceExpression e =>
+      expression.objectPropertyReferenceExpression = e.toProtobuf(),
     _ => throw UnsupportedError("${object.runtimeType} not supported"),
-  }; 
+  };
 
   return expression;
 }
